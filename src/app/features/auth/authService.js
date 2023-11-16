99403274eb4357cafba21e5bbe12b1b9a51bd7b9
Mohamed Shiyas B <147586484+Shiyas9961@users.noreply.github.com>
2023-11-16 19:44:09 +0530

    Update authService.js
    auth url changed

--- src/app/features/auth/authService.js
+++ src/app/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5001/api/users'
+const API_URL = 'https://goals-yr91.onrender.com/api/users'
 
 const register = async (userData) => {
     const response = await axios.post(API_URL,userData)
